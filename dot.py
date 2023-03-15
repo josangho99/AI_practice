89ec1d1e7f97ff402af21b78a31040d7444f0a0e
DESKTOP-3URSLJ2\josangho <josangho99@gmail.com>
2023-03-15 12:15:30 +0900

    for문 예시 추가

--- dot.py
+++ dot.py
@@ -6,12 +6,16 @@ a = np.array([[1],
 b= np.array([[3,2,1]])
 
 def dot(A,B):
-    result = np.dot(A,B)
+    result = np.dot(A,B) # np.dot 행렬곱
     return result
 
 print(dot(a,b))
 print(dot(b,a))
-print(a@b)
+print(a@b) # A@B 내적
 print(b@a)
-print(a*b)
+print(a*b) # A*B, 원소 곱
 print(b*a)
+
+print(a.shape) #(row,column)
+print(a.shape[0]) # row
+print(a.shape[1]) # column
---
+++ for.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+A = np.array([[[6],[7],[8],[9]]])
+
+for i,item in enumerate(A):
+    print(i)
+    print(item)
